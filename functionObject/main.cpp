50f9acef9b630ce4109d6c6746f1792b41b1a76c
78472 <784723153@11.com>
2022-02-10 17:44:56 +0800

    函数

--- functionObject/main.cpp
+++ functionObject/main.cpp
@@ -29,15 +29,15 @@ class memberAddClass{
 private:
     int value = 100;
 public:
-    int add(int a, int b){
+    int add(int a, int b) const{
         return a + b + value;
     }
 
-    int mul(int a, int b){
+    int mul(int a, int b) const{
         return a * b + value;
     }
 
-    int div(int a, int b){
+    int div(int a, int b) const{
         return a / b + value;
     }
 
@@ -45,50 +45,53 @@ public:
         return a - b;
     }
 
-    int addWithNum(int a, int b){
+    int addWithNum(int a, int b) const{
         return a + b + value;
     }
 
-    int addWithNum(int a, int b, int c){
+    int addWithNum(int a, int b, int c) const{
         return a + b + c + value;
     }
 };
 
 
-//普通函数指针
+//普通函数：函数名是函数的地址，即函数指针变量的值。
 void normal_func_pointer(){
 
-    int(*fp)(int, int);     //定义一个函数指针变量
-    fp = normalAddFunction;       //给函数指针变量赋值
+    int(*fp)(int, int);        //定义一个函数指针变量
+    fp = normalAddFunction;    //给函数指针变量赋值
     std::cout << "fp(1, 2): " << fp(1, 2) << std::endl;
 
-    typedef int(*p2func)(int, int);     //定义一个函数指针类型
-    p2func fp1 = normalAddFunction;     //创建一个函数指针变量，并为其赋值
+    typedef int(*addFunc)(int, int);     //定义一个函数指针类型
+    addFunc fp1 = normalAddFunction;     //创建一个函数指针变量，并为其赋值
     std::cout << "fp1(1, 2): " << fp1(1, 2) << std::endl;
 
-    using p2funciii = int(*)(int, int);     //定义一个函数指针类型
-    p2funciii fp2 = normalAddFunction;      //创建一个函数指针变量，并为其赋值
+    using addFunc2 = int(*)(int, int);     //定义一个函数指针类型
+    addFunc2 fp2 = normalAddFunction;      //创建一个函数指针变量，并为其赋值
     std::cout << "fp2(1, 2): " << fp2(1, 2) << std::endl;
 }
 
 
-//function type wrapper
+//function type wrapper：其值可以为普通函数，函数对象，lamda
 void normal_functionObject_cpp(){
 
     std::function<int(int,int)> f;
 
-    f = normalAddFunction;    //普通函数
+    f = normalAddFunction;      //普通函数
     std::cout << "addFunction: " << f(1, 2) << std::endl;
 
-    f = addFunctionObject();    //创建一个函数对象实体
+    f = addFunctionObject();    //函数对象
     std::cout << "addFunctionObject: " << f(1, 2) << std::endl;
+    std::cout << "addFunctionObject()(1, 2): " << addFunctionObject()(1, 2) << endl;
+    std::cout << "addFunctionObject().operator()(1, 2): " << addFunctionObject().operator()(1, 2) << endl;
 
-    f = [](int a, int b) ->int {return a + b;};     //lamda
+    f = [](int a, int b) ->int {return a + b;};   //lamda
     std::cout << "lambda: " << f(1, 2) << std::endl;
 }
 
 
-void member_funciton_cpp(){
+//成员函数：调用成员函数需要对象，参数
+void member_function_cpp(){
 
 //静态成员函数本质上就是普通函数
     using staticMemberFunction = int(*)(int, int);
@@ -96,16 +99,18 @@ void member_funciton_cpp(){
     std::cout << "smf(1, 2): " << smf(1, 2) << std::endl;
 
 //成员函数指针
-    int (memberAddClass::*mfp)(int, int);   //定义一个成员函数指针变量
-    mfp = &memberAddClass::add;               //给成员函数指针变量赋值，对于成员函数c++标准强制要求加上'&'
+    int (memberAddClass::*mfp)(int, int) const;   //定义一个成员函数指针变量
+    mfp = &memberAddClass::add;                   //给成员函数指针变量赋值，对于成员函数c++标准强制要求加上'&'
 
-    using mfpiii_fpType = int(memberAddClass::*)(int, int, int);   //定义成员函数指针类型
+    using mfpiii_fpType = int(memberAddClass::*)(int, int, int)const;   //定义成员函数指针类型
     mfpiii_fpType mfp1 = &memberAddClass::addWithNum;
 
+    //调用方法1
     memberAddClass a;
     int result = (a.*mfp)(1, 2);        //通过对象，成员函数指针来调用成员函数
     std::cout << "(a.*mfp)(1, 2): " << result << std::endl;
 
+    //调用方法2
     std::function<int(int, int)> f;
     f = std::bind(mfp, a, std::placeholders::_1, std::placeholders::_2);    //将成员函数和对象绑定到一起
     std::cout << "addClass::addWithNum(int, int): " << f(1, 2) << std::endl;
@@ -116,7 +121,7 @@ void member_funciton_cpp(){
 
 
 void map_function_call(){
-    using memfp = int(memberAddClass::*)(int, int);   //声明成员函数指针变量类型
+    using memfp = int(memberAddClass::*)(int, int)const;   //声明成员函数指针变量类型
 
 //成员函数map
     std::map<string, memfp> fmap;
@@ -143,10 +148,10 @@ void map_function_call(){
     }
 }
 
-
+//成员函数调用
 void mem_func_call(){
     //定义memberAddClass的成员函数类型, 成员函数的默认第一实参类型就是定义该成员函数的类
-    using memfunctionType = int(memberAddClass::*)(int, int);
+    using memfunctionType = int(memberAddClass::*)(int, int)const;
 
     memberAddClass a;
     std::function<int(memberAddClass&, int, int)> mf;  //function模板指明的函数类型：第一实参为memberAddClass类对象
@@ -162,16 +167,24 @@ public:
     print(){
         std::cout << "print constructor() " << std::endl;
     }
+
+    ~print(){
+        std::cout << "print destructor() " << std::endl;
+    }
+
+    void echo(){
+        std::cout << "message from print" << std::endl;
+    }
 };
 
+//模板本质上是一种编译期替换
 template<typename T>
 void test(){
-    T();
+    T().echo();
 }
 
 
 int main(int argc, char* argv[]){
-//    mem_func_call();
 
     test<print>();
 
