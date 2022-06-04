d88bfd085649ba7a3654c69d80e43f54dbd45f41
lhy <784723153@qq.com>
2022-06-04 18:01:37 +0800

    修改functionObject

--- stl/functionObject/main.cpp
+++ stl/functionObject/main.cpp
@@ -7,189 +7,284 @@
 
 using namespace std;
 
+
+/*
+ * callable object
+ *      1.一个函数, 接受额外传入的args作为实参
+ *      2.一个指向成员函数的指针,当通过对象调用它, 该对象被传递为第一实参(必须是个reference或pointer),  其它实参则一一对应成员函数的参数
+ *      3.一个函数对象, 附带的args被传递作为实参
+ *      4.一个lamda, 严格的说它是一种函数对象
+ */
+
+
 //普通函数
-int normalAddFunction(int a, int b){
+int addNormal(int a, int b){
     return a + b;
 }
 
-//函数对象
-class addFunctionObject{
+//函数对象：重载函数调用()
+class AddFuncObject{
+private:
+    string printStr;
 public:
+    explicit AddFuncObject(string str): printStr(std::move(str)){}
+
+    AddFuncObject(const AddFuncObject& af){
+        std::cout << "copy ctr AddFuncObject" << std::endl;
+    }
+
+    ~AddFuncObject(){ std::cout << "~AddFuncObject：" << printStr << std::endl; }
+
     int operator()(int a, int b){
+        std::cout << "operator()(int a, int b): " << printStr << std::endl;
         return a + b;
     }
 
     int operator()(int a, int b, int c){
+        std::cout << "operator()(int a, int b, int c): " << printStr << std::endl;
         return a + b + c;
     }
 };
 
 //成员函数
-class memberAddClass{
+class Arithmetic{
 private:
-    int value = 100;
+    string printStr;
 public:
-    int add(int a, int b) const{
-        return a + b + value;
+    explicit Arithmetic(string str) : printStr(std::move(str)){}
+
+    ~Arithmetic(){ std::cout << "~Arithmetic：" << printStr << std::endl; }
+
+    int add(int a, int b){
+        std::cout << "add(int a, int b): " << printStr << std::endl;
+        return a + b;
     }
 
-    int mul(int a, int b) const{
-        return a * b + value;
-    }
-
-    int div(int a, int b) const{
-        return a / b + value;
-    }
-
-    static int sub(int a, int b){
+    int sub(int a, int b){
+        std::cout << "sub(int a, int b): " << printStr << std::endl;
         return a - b;
     }
 
-    int addWithNum(int a, int b) const{
-        return a + b + value;
+    int mul(int a, int b) const{
+        std::cout << "mul(int a, int b): " << printStr << std::endl;
+        return a * b;
     }
 
-    int addWithNum(int a, int b, int c) const{
-        return a + b + c + value;
-    }
-};
-
-
-//普通函数：函数名是函数的地址，即函数指针变量的值。
-void normal_func_pointer(){
-
-    int(*fp)(int, int);        //定义一个函数指针变量
-    fp = normalAddFunction;    //给函数指针变量赋值
-    std::cout << "fp(1, 2): " << fp(1, 2) << std::endl;
-
-    typedef int(*addFunc)(int, int);     //定义一个函数指针类型
-    addFunc fp1 = normalAddFunction;     //创建一个函数指针变量，并为其赋值
-    std::cout << "fp1(1, 2): " << fp1(1, 2) << std::endl;
-
-    using addFunc2 = int(*)(int, int);     //定义一个函数指针类型
-    addFunc2 fp2 = normalAddFunction;      //创建一个函数指针变量，并为其赋值
-    std::cout << "fp2(1, 2): " << fp2(1, 2) << std::endl;
-}
-
-
-//function type wrapper：其值可以为普通函数，函数对象，lamda
-void normal_functionObject_cpp(){
-
-    std::function<int(int,int)> f;
-
-    f = normalAddFunction;      //普通函数
-    std::cout << "addFunction: " << f(1, 2) << std::endl;
-
-    f = addFunctionObject();    //函数对象
-    std::cout << "addFunctionObject: " << f(1, 2) << std::endl;
-    std::cout << "addFunctionObject()(1, 2): " << addFunctionObject()(1, 2) << endl;
-    std::cout << "addFunctionObject().operator()(1, 2): " << addFunctionObject().operator()(1, 2) << endl;
-
-    f = [](int a, int b) ->int {return a + b;};   //lamda
-    std::cout << "lambda: " << f(1, 2) << std::endl;
-}
-
-
-//成员函数：调用成员函数需要对象，参数
-void member_function_cpp(){
-
-//静态成员函数本质上就是普通函数
-    using staticMemberFunction = int(*)(int, int);
-    staticMemberFunction smf = &memberAddClass::sub;    //静态成员函数
-    std::cout << "smf(1, 2): " << smf(1, 2) << std::endl;
-
-//成员函数指针
-    int (memberAddClass::*mfp)(int, int) const;   //定义一个成员函数指针变量
-    mfp = &memberAddClass::add;                   //给成员函数指针变量赋值，对于成员函数c++标准强制要求加上'&'
-
-    using mfpiii_fpType = int(memberAddClass::*)(int, int, int)const;   //定义成员函数指针类型
-    mfpiii_fpType mfp1 = &memberAddClass::addWithNum;
-
-    //调用方法1
-    memberAddClass a;
-    int result = (a.*mfp)(1, 2);        //通过对象，成员函数指针来调用成员函数
-    std::cout << "(a.*mfp)(1, 2): " << result << std::endl;
-
-    //调用方法2
-    std::function<int(int, int)> f;
-    f = std::bind(mfp, a, std::placeholders::_1, std::placeholders::_2);    //将成员函数和对象绑定到一起
-    std::cout << "addClass::addWithNum(int, int): " << f(1, 2) << std::endl;
-
-    f = std::bind(mfp1, a, std::placeholders::_1, std::placeholders::_2, 100);
-    std::cout << "addClass::addWithNum(int, int, int): " << f(1, 2) << std::endl;
-}
-
-
-void map_function_call(){
-    using memfp = int(memberAddClass::*)(int, int)const;   //声明成员函数指针变量类型
-
-//成员函数map
-    std::map<string, memfp> fmap;
-    fmap.insert(pair<string, memfp>("add", &memberAddClass::add));
-    fmap.insert(pair<string, memfp>("mul", &memberAddClass::mul));
-    fmap.insert(pair<string, memfp>("div", &memberAddClass::div));
-
-//对象map
-    std::map<string, memberAddClass> memMap;
-    memberAddClass a;
-    memMap.insert(pair<string, memberAddClass>("mem", a));
-
-    string objectOption = "mem";
-    string methodOption = "add";
-    int input1 = 1;
-    int input2 = 2;
-
-    auto obj = memMap.find(objectOption);
-    if(obj != memMap.end()){
-        auto f = fmap.find(methodOption);
-        if(f != fmap.end()){
-            std::cout << "===>: " << (obj->second.*f->second)(input1, input2) << std::endl;
-        }
-    }
-}
-
-//成员函数调用
-void mem_func_call(){
-    //定义memberAddClass的成员函数类型, 成员函数的默认第一实参类型就是定义该成员函数的类
-    using memfunctionType = int(memberAddClass::*)(int, int)const;
-
-    memberAddClass a;
-    std::function<int(memberAddClass&, int, int)> mf;  //function模板指明的函数类型：第一实参为memberAddClass类对象
-    memfunctionType f = &memberAddClass::add;
-    mf = f;
-    std::cout << "a.add(1, 2): " << mf(a, 1, 2) << std::endl;   //成员函数的调用方法1（更接近本质的调用）
-    std::cout << "a.add(1, 2): " << (a.*f)(1, 2) << std::endl;  //成员函数的调用方法2
-}
-
-
-class print{
-public:
-    print(){
-        std::cout << "print constructor() " << std::endl;
+    int div(int a, int b) const{
+        std::cout << "div(int a, int b): " << printStr << std::endl;
+        return a / b;
     }
 
-    ~print(){
-        std::cout << "print destructor() " << std::endl;
+    static void show(int a, int b){
+        std::cout << "a: " << a << ", b " << b <<std::endl;
     }
 
-    void echo(){
-        std::cout << "message from print" << std::endl;
+    void setPrintStr(string str){
+        printStr = std::move(str);
     }
 };
 
-//模板本质上是一种编译期替换
-template<typename T>
-void test(){
-    T().echo();
+
+/**
+ * 函数名是函数所在代码段的地址，即函数指针变量的值。
+ * 4种声明函数指针变量的方式：本质上是2种：函数指针；函数覆盖器
+ *      1. int(*nfp)(int, int);
+ *      2. typedef int(*addFunc)(int, int);
+ *      3. using addFunc2 = int(*)(int, int);
+ *      4. using ArithmeticFunc = std::function<int(int, int)>
+ */
+void callNormalFunc(){
+    //直接调用
+    std::cout << "add(1, 2): " << addNormal(1, 2) << std::endl;
+
+    //通过函数指针变量调用
+    int(*nfp)(int, int);          //声明
+    nfp = addNormal;              //赋值
+    std::cout << "nfp(1, 2): " << nfp(1, 2) << std::endl;
+
+    typedef int(*addFunc)(int, int);    //定义类型
+    addFunc nfp1 = addNormal;           //声明并赋值
+    std::cout << "nfp1(1, 2): " << nfp1(1, 2) << std::endl;
+
+    using addFunc2 = int(*)(int, int);     //定义类型
+    addFunc2 nfp2 = addNormal;             //声明并赋值
+    std::cout << "nfp2(1, 2): " << nfp2(1, 2) << std::endl;
+
+    //函数覆盖器
+    using ArithmeticFunc = std::function<int(int, int)> ;
+    ArithmeticFunc add_normal = addNormal;
+    std::cout << "add_normal(1, 2): " << add_normal(1, 2) << std::endl;
 }
 
+
+/**
+ * 通过函数对象来完成函数调用:
+ *     1. 创建对象：
+ *     2. 调用对象的函数(重载函数调用符'()')
+ */
+void callFunctionObject(){
+    AddFuncObject afo("AddFuncObject-afo");
+    afo(1, 2);
+    afo(1, 2, 3);
+    afo.operator()(1, 2);
+    afo.operator()(1, 2, 3);
+    std::cout << "--------------" << std::endl;
+
+    //创建临时对象来调用函数, 临时对象使用完会被立即析构
+    AddFuncObject("AddFuncObject1")(1, 2);
+    AddFuncObject("AddFuncObject2")(1, 2, 3);
+    AddFuncObject("AddFuncObject3").operator()(1, 2);
+    AddFuncObject("AddFuncObject4").operator()(1, 2, 3);
+    std::cout << "--------------" << std::endl;
+
+    //函数覆盖器
+    using ArithmeticFunc = std::function<int(int, int)> ;
+    ArithmeticFunc add_object = AddFuncObject("AddFuncObject5");
+    add_object(1, 2);
+}
+
+
+/**
+ *      1. 定义并赋值指向类函数的指针变量
+ *      2. 通过对象，指向类函数的指针变量来访问该对象的函数
+ */
+void callMemberFunc(){
+    //静态成员函数：普通函数 + 类作用域
+    using staticMemberFunction = void(*)(int, int);
+    staticMemberFunction smf = Arithmetic::show;    //静态成员函数: 携带类作用域的普通函数
+    smf(1, 2);
+
+    //成员函数
+    using ArithmeticMemberFunc = int(Arithmetic::*)(int, int);   //指向类成员函数的指针，定义时携带类作用域
+    ArithmeticMemberFunc add = &Arithmetic::add;                 //给成员函数指针变量赋值，对于成员函数c++标准强制要求加上'&'
+    ArithmeticMemberFunc sub = &Arithmetic::sub;
+
+    using ArithmeticMemberFuncConst = int(Arithmetic::*)(int, int) const;
+    ArithmeticMemberFuncConst mul = &Arithmetic::mul;
+    ArithmeticMemberFuncConst div = &Arithmetic::div;
+
+    //通过对象调用方法
+    Arithmetic a("Arithmetic a");
+    (a.*add)(1, 2);
+    (a.*sub)(1, 2);
+
+    Arithmetic b("Arithmetic b");
+    (b.*mul)(1, 2);
+    (b.*div)(1, 2);
+    std::cout << "-----------" << std::endl;
+
+    using ArithmeticFunc = std::function<int(int, int)> ;
+    ArithmeticFunc add_mem_bind = std::bind(add, a, std::placeholders::_1, std::placeholders::_2);
+    ArithmeticFunc add_mem_lamda = [&](int first, int second){
+        return b.add(first, second);
+    };
+    add_mem_bind(1, 2);
+    add_mem_lamda(1, 2);
+    std::cout << "-----------" << std::endl;
+
+    //函数覆盖器
+    using ArithmeticFuncMember = std::function<int(Arithmetic&, int, int)>;
+    ArithmeticFuncMember add_member = &Arithmetic::add;
+    add_member(a, 1, 2);
+}
+
+
+/**
+ *  function type wrapper：其值可以为普通函数，函数对象，lamda
+ */
+void functionWrapper(){
+    using ArithmeticFunc = std::function<int(int, int)>;
+
+    //普通函数
+    ArithmeticFunc add_normal = addNormal;
+    std::cout << "add_normal: " << add_normal(1, 2) << std::endl;
+
+    //函数对象
+    ArithmeticFunc add_object = AddFuncObject("AddFuncObject");
+    add_object(1, 2);
+
+    //lamda
+    ArithmeticFunc add_lamda = [](int a, int b){
+        return a + b;
+    };
+    std::cout << "add_lamda: " << add_lamda(1, 2) << std::endl;
+
+    //成员函数
+    Arithmetic a("Arithmetic_member");
+    //方式1
+    ArithmeticFunc add_mem_bind = std::bind(&Arithmetic::add, a, std::placeholders::_1, std::placeholders::_2);
+    //方式2
+    ArithmeticFunc add_mem_lamda = [&](int first, int second)-> int{
+        return a.add(first, second);
+    };
+    add_mem_bind(1, 2);
+    add_mem_lamda(1, 2);
+
+    //方式3
+    using ArithmeticFuncMember = std::function<int(Arithmetic&, int, int)>;
+    ArithmeticFuncMember add_member = &Arithmetic::add;
+    add_member(a, 1, 2);
+}
+
+
+
+void map_func_call(string& key){
+    using ArithmeticFunc = std::function<int(int, int)>;
+    Arithmetic a("Arithmetic_a");
+
+    //成员函数map
+    std::map<string, ArithmeticFunc> arithmeticFuncMap;
+    arithmeticFuncMap.insert(std::make_pair("addNormal", addNormal));
+    arithmeticFuncMap.insert(std::make_pair("addFuncObject",AddFuncObject("AddFuncObject")));
+    std::cout << "-------------" << std::endl;
+
+    arithmeticFuncMap.insert(std::make_pair("lamda", [](int a, int b){
+        return a + b;
+    }));
+    arithmeticFuncMap.insert(std::make_pair("member", [&](int first, int second){
+        return a.add(first, second);
+    }));
+
+    a.setPrintStr("Arithmetic_aaa");
+
+    auto obj = arithmeticFuncMap.find(key);
+    if(obj != arithmeticFuncMap.end()){
+        std::cout << obj->second(1, 2) << std::endl;
+    }
+}
+
+
+void map_memberFunc_call(Arithmetic& arithmetic, string& key){
+    using ArithmeticFuncMember = std::function<int(Arithmetic&, int, int)>;
+
+    std::map<string, ArithmeticFuncMember> mapFunc;
+    mapFunc.insert(std::make_pair("add", &Arithmetic::add));
+    mapFunc.insert(std::make_pair("sub", &Arithmetic::sub));
+    mapFunc.insert(std::make_pair("mul", &Arithmetic::mul));
+    mapFunc.insert(std::make_pair("div", &Arithmetic::div));
+
+    auto obj = mapFunc.find(key);
+    if(obj != mapFunc.end()){
+        obj->second(arithmetic, 1, 2);
+    }
+}
+
+
+void funcAssign(){
+    using ArithmeticFunc = std::function<int(int, int)>;
+
+    ArithmeticFunc add = AddFuncObject("AddFuncObject");
+}
 
 int main(int argc, char* argv[]){
+//    Arithmetic a("Arithmetic_a");
+//    string key = "add";
 
-    test<print>();
 
-    while(true)
-        std::this_thread::sleep_for(std::chrono::seconds(10));
+//    map_memberFunc_call(a, key);
+
+    callFunctionObject();
+
     return 0;
 }
 
