b2120e48c4c068b105404481ba80abcd8354294d
lhy <784723153@qq.com>
2022-06-05 20:13:50 +0800

    shared pointer

--- stl/smartPointer/sharePointer.h
+++ stl/smartPointer/sharePointer.h
@@ -6,12 +6,14 @@
 #define STL_SHAREPOINTER_H
 
 /*
- * 所有smart pointer class都被定义于头文件<memory>内
- * 如何确保“pointer的寿命”和“其所指向的对象的寿命”一致？
- *      shared_ptr实现共享式拥有的概念，多个smart pointer可以指向相同的对象，该对象和其相关资源
+ *  所有smart pointer class都被定义于头文件<memory>内
+ *
+ *  1. 使用pointer的理由之一是在惯常的作用域边界之外拥有reference语义。
+ *  2. 如何确保“pointer的寿命”和“其所指向的对象的寿命”一致？
+ *      shared_ptr: 实现共享式拥有的概念，多个smart pointer可以指向相同的对象，该对象和其相关资源
  *      会在“最后一个reference被销毁”时释放。
  *
- *      unique_ptr实现独占式拥有或严格拥有的概念，保证同一时间内只有一个smart pointer指向该对象，
+ *      unique_ptr: 实现独占式拥有或严格拥有的概念，保证同一时间内只有一个smart pointer指向该对象，
  *      可以移交所有权。
  */
 
