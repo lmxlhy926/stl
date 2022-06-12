6b906498b87e99d0aa8d9ca3cd34f13f0f67d1a2
lhy <784723153@qq.com>
2022-06-12 19:18:29 +0800

    time

--- mudo/time/lhytime.h
+++ mudo/time/lhytime.h
@@ -7,16 +7,20 @@
 
 /*
  * time_t和struct tm之间的相互转换
-    time_t time(time_t *seconds);
-    time_t mktime(struct tm *timeptr);
-    struct tm *gmtime(const time_t *timer);
-    struct tm *localtime(const time_t *timer);
-
+    产生时间戳
+        time_t time(time_t *seconds);
+    time_t --> struct tm
+        struct tm *gmtime(const time_t *timer);         //格林尼治时间
+        struct tm *localtime(const time_t *timer);      //本地时区表示
+    struct tm --> time_t
+        time_t mktime(struct tm *timeptr);
 
  * 字符串化的格式表示
-    char *ctime(const time_t *timer);
-    char *asctime(const struct tm *timeptr);
-    size_t strftime(char *str, size_t maxsize, const char *format, const struct tm *timeptr);
+    time_t
+        char *ctime(const time_t *timer);
+    struct tm
+        char *asctime(const struct tm *timeptr);
+        size_t strftime(char *str, size_t maxsize, const char *format, const struct tm *timeptr);
 
 注释
     ctime(),asctime()的字符串形式  "Www Mmm dd hh:mm:ss yyyy\n\0"。
---
+++ mudo/time/main.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include "lhytime.h"
+
+int main(int argc, char* argv[]){
+    timetest();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
