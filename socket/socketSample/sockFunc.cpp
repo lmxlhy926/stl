87591951150adf4004c255864f5430d02995d155
lhy <784723153@qq.com>
2022-09-04 22:34:47 +0800

    socket地址转换

---
+++ socket/socketSample/sockFunc.cpp
@@ -0,0 +1,65 @@
+//
+// Created by 78472 on 2022/9/4.
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+/*
+    将<主机名、主机地址>， <服务名、端口号>的字符串表示  <---->   网络字节序的地址结构
+
+    提供<主机名、主机地址>， <服务名、端口号>， 控制结构，返回指向地址列表的指针
+    int getaddrinfo(const char *host, const char *service,
+                    const struct addrinfo *hints,
+                    struct addrinfo **result)
+
+    //释放返回的地址列表
+    void freeaddrinfo(struct addrinfo *result);
+
+    //将错误码转换为字符串表示
+    const char *gai_strerror(int errcode);
+
+    将网络字节序的套接字地址转换为相应的主机、服务名字符串
+    int getnameinfo(const struct sockaddr *sa, socklen_t, salen,
+                    char *host, size_t hostlen,
+                    char *service, size_t servlen, int flags);
+
+ */
+
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        fprintf(stderr, "usage: %s <domain name>\n", argv[0]);
+        exit(0);
+    }
+
+    //控制套接字地址列表的返回
+    struct addrinfo hint{};
+    memset(&hint, 0, sizeof(hint));
+    hint.ai_family = AF_INET;           //只返回IPV4地址
+    hint.ai_socktype = SOCK_STREAM;     //只返回连接类型
+
+    struct addrinfo *p, *listp;
+    int rc;
+    if((rc = getaddrinfo(argv[1], nullptr, &hint, &listp)) != 0){
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(rc));
+        exit(1);
+    }
+
+    char buf[INET_ADDRSTRLEN];
+    int flags = NI_NUMERICHOST;     //将IP地址转换为点分十进制表示
+    for(p = listp; p != nullptr; p = p->ai_next){
+        //提取网络字节序地址信息，转换为地址、端口字符串
+        getnameinfo(p->ai_addr, p->ai_addrlen, buf, INET_ADDRSTRLEN, nullptr, 0, flags);
+        printf("%s\n", buf);
+
+    }
+
+    freeaddrinfo(listp);
+
+    return 0;
+}
