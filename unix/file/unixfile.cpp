4cc7bc319e75d3a9ad7fa8b1e79329b61a282e52
lhy <784723153@qq.com>
2022-12-29 23:31:30 +0800

    file

--- unix/file/unixfile.cpp
+++ unix/file/unixfile.cpp
@@ -14,33 +14,37 @@ using namespace std;
 /*
  * oflag参数：
  *      必选且只能选定一个：O_RDONLY, O_WRONLY, O_RDWR, O_EXEC
- *      常用可选项：O_TRUNC, O_APPEND, O_CREAT,
- * 读取文件所有内容并打印
- * oflag:
- *      O_TRUNC : 打开时将文件长度截断为0
- *      O_APPEND：在每次写操作之前，将文件偏移量设置在文件的当前结尾处
- *      都没有指定，则打开文件，文件偏移量指示在文件开头处，每次从当前偏移量处开始进行操作。
+ *      常用可选项:  O_CREAT     文件不存在则按照指定的文件权限创建一个新文件
+ *                 O_TRUNC     打开时将文件长度截断为0
+ *                 O_APPEND    在每次写操作之前，将文件偏移量设置在文件的当前结尾处
+ *                 都没有指定，则打开文件，文件偏移量指示在文件开头处，每次从当前偏移量处开始进行操作。
  */
-void file1(){
-    int fd = open("/home/lhy/test/a.txt", O_RDWR | O_CREAT, 0666);
+
+/*
+ * 1. 确定文件长度
+ * 2. 读取文件，并打印文件内容
+ */
+void readFileContent(){
+    int fd = openat(AT_FDCWD, "a.txt", O_RDWR | O_CREAT, 0666);
     off_t size = lseek(fd, 0, SEEK_END);    //定位到尾端确定文件长度
-    lseek(fd, 0, SEEK_SET);     //定位到开头准备读取文件
+    std::cout << "size: " << size << std::endl;
+    lseek(fd, 0, SEEK_SET);                 //定位到开头准备读取文件
     if(size > 0){
-        char buf[size + 1];
+        char buf[size];
         ssize_t nRead = read(fd, buf, size);
-        std::cout << string(buf, nRead) << std::endl;
+        std::cout << string(buf, nRead) << ", size: " << nRead;
     }
     close(fd);
 }
 
 
 /*
- * 文件偏移量：读、写操作都从当前文件偏移量处开始，并使偏移量增加多读写的字节数。
+ * 文件偏移量：读、写操作都从当前文件偏移量处开始，并使偏移量增加读写的字节数。
  * 开始位置在0，读取5个字节数据，位置在5；从位置5开始写入5个字节数据，位置指向10
  */
-void file2(){
+void positionForward(){
     char readBuf[1024];
-    int fd = open("/home/lhy/test/a.txt", O_RDWR | O_CREAT, 0666);
+    int fd = openat(AT_FDCWD, "a.txt", O_RDWR | O_CREAT, 0666);
     printf("pos: %d\n", static_cast<int>(lseek(fd, 0, SEEK_CUR)));
     read(fd, readBuf, 5);
     printf("pos: %d\n", static_cast<int>(lseek(fd, 0, SEEK_CUR)));
@@ -95,6 +99,17 @@ void stdinout(){
     write(STDERR_FILENO, afterss.str().c_str(), afterss.str().size());
 }
 
+/*
+ * command > file   ：将输出重定向到file
+ * command < file   ：将输入重定向到file
+ * command >> file  ：将输出以追加的方式重定向到file
+ * n>&m             ：将描述符n重定向到m
+ */
+void redirectionTest(){
+    string str = "hello";
+    write(STDOUT_FILENO, str.c_str(), 5);
+    write(STDERR_FILENO, str.c_str(), 5);
+}
 
 /*
  *  dup, dup2
@@ -123,6 +138,6 @@ void dupTest(){
 }
 
 int main(int argc, char* argv[]){
-    dupTest();
+    positionForward();
     return 0;
 }
