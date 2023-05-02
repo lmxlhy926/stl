df783c4554fff4d5f52039e65ac02f8c3841c29b
lhy <784723153@qq.com>
2023-05-02 23:08:48 +0800

    stdio

--- unix/02file/stdio.cpp
+++ unix/02file/stdio.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cstdio>
 #include <unistd.h>
+#include <array>
 
 //读取字符
 void readChar(){
@@ -170,7 +171,7 @@ void readFromLineBufStdin(){
     }
 }
 
-
+//读写转换时要加定位操作
 void readWriteFromLineBuf(){
     FILE* fp = fopen("/home/lhy/project/stl/unix/02file/a.txt", "r+");
     char buffer[1024]{};
@@ -244,41 +245,146 @@ void readFromFullbuf(){
 }
 
 
-int main(int argc, char* argv[]){
-    write2fullbuf();
+/*
+    缓冲区内容<---->文件实际内容
+*/
+void readWriteFromFullbuf(){
+    FILE* fp = fopen("/home/lhy/project/stl/unix/02file/a.txt", "r+");
+    char buffer[1024]{};
+    setvbuf(fp, buffer, _IOFBF, 1024);
+    if(fp != nullptr){
+        char buf[1024]{};
+        if(fgets(buf, 1024, fp) != nullptr){
+            printf("readStr: %s", buf);
+            fflush(stdout);
+        }
 
-    return 0;
+        fseek(fp, 3, SEEK_SET);
+        if(fgets(buf, 1024, fp) != nullptr){
+            printf("readStr: %s", buf);
+            fflush(stdout);
+        }
+
+        fseek(fp, 5, SEEK_SET);
+        if(fgets(buf, 1024, fp) != nullptr){
+            printf("readStr: %s", buf);
+            fflush(stdout);
+        }
+        
+        long nowPosition = ftell(fp);
+        rewind(fp);
+        fputc('a', fp);
+        fputc('b', fp);
+        fputc('c', fp);
+        fseek(fp, nowPosition, SEEK_SET);
+        fputc('a', fp);
+        fputc('b', fp);
+        fputc('c', fp);
+        fflush(fp);
+
+        /*
+            从文件读取内容，是把文件内容读取到缓冲区中。
+            如果文件内容发生改变，但是此时并没有读取文件内容到缓冲区中，则缓冲区中还是以前的内容。
+            只有再次读取文件内容，缓冲区内容才能反映文件内容变化。
+        */
+        rewind(fp);
+        while(true){
+            if(fgets(buf, 1024, fp) != nullptr){
+                printf("readStr: %s", buf);
+                fflush(stdout);
+                fseek(fp, 0, SEEK_CUR);
+            }else{
+                break;
+            }
+        }
+
+        rewind(fp);
+        while(true){
+            if(fgets(buf, 1024, fp) != nullptr){
+                printf("readStr: %s", buf);
+                fflush(stdout);
+                fseek(fp, 0, SEEK_CUR);
+            }else{
+                break;
+            }
+        }
+        fclose(fp);
+    }
 }
 
 
+/*
+    freopen函数在一个指定的流上打开一个指定的文件，若该流已经打开，则先关闭该流。
+    若该流已经定向，则使用freopen清除该定向。
+    此函数一般用于将一个指定的文件打开为一个预定义的流：stdin, stdout, stderr
+    可以用于I/O重定向
+*/
+void freopenTest(){
+    FILE* fp = freopen("/home/lhy/project/stl/unix/02file/a.txt", "w+", stdout);
+    if(fp != nullptr){
+        while(true){
+            char buf[1024]{};
+            if(fgets(buf, 1024, stdin) != nullptr){
+                fprintf(stdout, "readStr: %s", buf);
+                fflush(stdout);
+            }else{
+                break;
+            }
+        }
+    }
+}
+
+/*
+    fdopen函数取一个已有的文件描述符，并使一个标准的I/O流与该描述符相结合。
+    对于fdopen, type参数的意义稍有区别，因为该描述符已经被打开。
+*/
+void fdopenTest(){
+    FILE* stdinfp = fdopen(0, "w+");
+    FILE* stdoutfp = fdopen(1, "w+");
+    while(true){
+        char buf[1024]{};
+        if(fgets(buf, 1024, stdinfp)){
+            fprintf(stdoutfp, "readStr: %s", buf);
+        }else{
+            break;
+        }
+    }
+}
 
 
+/*
+    snprintf:
+        最多容纳n-1个字符，最后一位一定是'\0'
+        多余的输出字符被丢弃
+*/
+void snprintfTest(){
+    char buf[5]{};
+    snprintf(buf, 5, "hello");
+    sleep(1);
+}
+ 
 
 
+/*
+    格式化输入：
+        0. 接收的内容是文本字符
+        1. 格式化字符串内指定解释的类型
+        2. 读取文本字符内容，依序将文本字符转换为相应的解释类型
+*/
+void scanfTest(){
+    FILE* fp = fopen("/home/lhy/project/stl/unix/02file/a.txt", "r+");
+    int a, b;
+    char buf[100]{};
+    fscanf(fp, "%d %d %s", &a, &b, buf);
+    fprintf(stdout, "a==%d, b==%d, c==%s\n", a, b, buf);
 
+    std::array<char, 10> Array{'1', '2', ' ', '3', '4', ' ', '5', '6', '7'};
+    sscanf(Array.data(), "%d %d %s", &a, &b, buf);
+    fprintf(stdout, "a==%d, b==%d, c==%s\n", a, b, buf);
+}
 
+int main(int argc, char* argv[]){
+    scanfTest();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
+}
