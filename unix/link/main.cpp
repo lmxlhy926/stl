6fec0ec1fe061fe3c6973a009718c240db1285ca
lhy <784723153@qq.com>
2023-06-28 23:36:08 +0800

    link

--- unix/link/main.cpp
+++ unix/link/main.cpp
@@ -1,16 +1,27 @@
 
+// 声明外部函数
 int add(int a, int b);
 int sub(int a, int b);
 void showResult(int result);
-extern int a, b;
-extern char* ptr;
+
+// 定义本地函数
+int mul(int a, int b){
+    return a * b;
+}
+
+// 声明外部全局变量
+extern int Global_a, Global_b;
+
+// 全局变量包含函数地址
+typedef int(*addFuncType)(int, int);
+addFuncType addFuncPtr = add;
+
 
 int main(int argc, char* argv[]){
-    if(ptr != nullptr){
-        showResult(0);
-    }
-    int c = 0, d = 0;
-    int result = add(a, b);
+    int local_a = 1, local_b = 2;
+    showResult(mul(local_a, local_b));
+    showResult(local_a + local_b);
+    int result = add(local_a, local_b);
     result = sub(result, 4);
     showResult(result);
     return 0;
