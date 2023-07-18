1ca4dff922b54aab2896333e37d3dae0d7acf0d7
lhy <784723153@qq.com>
2023-07-18 23:25:30 +0800

    exception

--- stl/06exception/main.cpp
+++ stl/06exception/main.cpp
@@ -1,14 +1,33 @@
 
 #include <iostream>
+#include <exception>
+#include <system_error>
+#include <future>
+#include <unistd.h>
 
 using namespace std;
 
+void handler1(){
+    throw logic_error("nihao logic error");
+}
+
+void handler2(){
+    handler1();
+}
+
+void handler3(){
+    handler2();
+}
+
 int main(int argc, char* argv[]){
-
-    std::cout << "in container main" << std::endl;
-
+    try{
+        handler2();
+    }catch(std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
+    
+    sleep(2);
     return 0;
-
 }
 
 
