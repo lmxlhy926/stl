e2655b8f89fc177f61e63056842fce7dde34469c
lhy <784723153@qq.com>
2024-02-27 23:40:54 +0800

    http

--- test/http/main.cpp
+++ test/http/main.cpp
@@ -10,18 +10,19 @@ using namespace std;
 
 class student{
     public:
+        //空参构造
         student(){
             std::cout << "empty constructor student ..." << std::endl;
         }
 
         //拷贝构造函数
         student(const student& stu){
-            std::cout << "copy student ..." << std::endl;
+            std::cout << "copy constructor student ..." << std::endl;
         }
 
         //move构造函数
         student(student&& stu){
-            std::cout << "move student ..." << std::endl;
+            std::cout << "move constructor student ..." << std::endl;
         }
 
         ~student(){
@@ -47,17 +48,31 @@ class student{
 };
 
 
+class person{
+private:
+    student _stu1;
+    student _stu2;
+public:
+    person(const student& stu): _stu1(stu){     //进入函数之前先拷贝构造，后空参构造
+        _stu2 = stu;    //赋值
+        std::cout << "person single param constructor ..." << std::endl;
+    }
+
+    ~person(){
+        std::cout << "destructor person ..." << std::endl;
+    }
+};
+
+
+
 void student_test(){
-    std::vector<student> vec;
-    student stu;
-
-    // stu + stu;
-
-    vec.push_back(stu);
-    vec.emplace_back(stu);
-
-
-
+    student stu;                    //空参构造
+    student stu2(stu);              //拷贝构造
+    student stu3(std::move(stu));   //move构造
+    stu = stu3;                    //拷贝赋值
+    stu = std::move(stu3);         //move赋值
+    std::cout << "----------" << std::endl;
+    person p(stu);
 }
 
 
@@ -77,7 +92,24 @@ std::vector<std::thread> threadVec;
 
 int main(int argc, char* argv[]){
 
-    student_test();
+   std::vector<int> vec;
+
+//创建
+
+
+//访问
+
+
+//插入
+
+
+//删除、清空
+
+
+//遍历
+
+
+//属性： 大小
 
 
 
