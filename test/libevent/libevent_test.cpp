60dd8297acad7a76d527cae092ef45832f16f444
lhy <784723153@qq.com>
2024-05-16 17:54:21 +0800

    libevent

--- test/libevent/libevent_test.cpp
+++ test/libevent/libevent_test.cpp
@@ -6,13 +6,30 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    int i = 0;
-    const char **methods = event_get_supported_methods();
-    printf("starting libevent %s. avilable methods are: \n", event_get_version());
-    while(methods[i] != nullptr){
-        printf("%s\n", methods[i++]);
+    struct event_config *cfg;
+    struct event_base *base;
+
+    cfg = event_config_new();
+    event_config_avoid_method(cfg, "select");
+    event_config_require_features(cfg, EV_FEATURE_ET);
+    event_config_set_flag(cfg, EVENT_BASE_FLAG_EPOLL_USE_CHANGELIST);
+    base = event_base_new_with_config(cfg);
+    event_config_free(cfg);
+
+    int features = event_base_get_features(base);
+    printf("using libevent with backend method %s.\n", event_base_get_method(base));
+    if(features & EV_FEATURE_ET){
+        printf("ev_feature_et\n");
+
+    }else if(features & EV_FEATURE_O1){
+        printf("ev_feature_o1\n");
+
+    }else if(features & EV_FEATURE_FDS){
+        printf("ev_feature_fds\n");
     }
 
+    event_base_priority_init(base, 3);
+    event_base_free(base);
 
     return 0;
 }
