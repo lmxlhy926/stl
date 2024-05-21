5f17cd7b827c8d865c5b4198a5abe3ff0a49868b
lhy <784723153@qq.com>
2024-05-21 17:53:32 +0800

    libevent

--- test/libevent/libevent_test.cpp
+++ test/libevent/libevent_test.cpp
@@ -8,61 +8,177 @@
 #include <thread>
 using namespace std;
 
-//创建event_base
-void event_base_create(){
-    struct event_config *cfg;
-    struct event_base *base;
 
-    //生成一个配置event_base的结构
-    cfg = event_config_new();
-    //调用函数配置event_config
-    //禁用指定的策略、指定指定的特性、设置特殊标志
-    event_config_avoid_method(cfg, "select");
-    event_config_require_features(cfg, EV_FEATURE_ET);
-    event_config_set_flag(cfg, EVENT_BASE_FLAG_EPOLL_USE_CHANGELIST);
-    //创建event_base
-    base = event_base_new_with_config(cfg);
-    //释放event_config
-    event_config_free(cfg);
-
-
-    //获取最终采用的方法
-    printf("using libevent with backend method %s.\n", event_base_get_method(base));
-    //获取目前所采用方法具有的特性
+void print_backend_property(struct event_base *base){
+    //确定采用的后端方法
+    printf("using libevent with backend method: %s\n", event_base_get_method(base));
+    //确定后端方法所具有的特性
     int features = event_base_get_features(base);
-    if(features & EV_FEATURE_ET){
-        printf("ev_feature_et\n");
+    if(features & EV_FEATURE_ET)
+        printf("backend fetature: EV_FEATURE_ET\n");
+    if(features & EV_FEATURE_O1)
+        printf("backend feature: EV_FEATURE_01\n");
+    if(features & EV_FEATURE_FDS)
+        printf("backend feature: EV_FEATURE_FDS\n");
+}
 
-    }else if(features & EV_FEATURE_O1){
-        printf("ev_feature_o1\n");
 
-    }else if(features & EV_FEATURE_FDS){
-        printf("ev_feature_fds\n");
-    }
+/**
+ * 创建event_base:
+ *      后端方法：
+ *              select、poll、epoll
+ *      后端方法具有的属性：
+ *              EV_FEATURE_ET、EV_FEATURE_FDS、EV_FEATURE_01
+ *              边沿触发、支持任意文件描述符不仅是socket描述符、添加、删除、确定激活事件的时间复杂度为O(1)。
+ *      事件可设定的优先级数目
+*/
+void event_base_create_test(){
+    struct event_base *base;
+    struct event_config *config;
 
-    //设置event_base优先级
-    event_base_priority_init(base, 3);
+    //构造一个配置结构
+    config = event_config_new();
+    //排除select方法
+    event_config_avoid_method(config, "select");
+    //需要满足的特性
+    event_config_require_features(config, EV_FEATURE_ET);
+    //创建event_base
+    base = event_base_new_with_config(config);
+    //释放event_config
+    event_config_free(config);
+
+    //打印后端方法及方法具有的属性
+    print_backend_property(base);
+    
+    //设置事件优先级数目
+    event_base_priority_init(base, EVENT_MAX_PRIORITIES);
+    //释放event_base
     event_base_free(base);
 }
 
 
+
 struct event_base *base;
-struct event *ev;
+struct event *event;
+
+/**
+ * event_base_
+*/
+void printExitStatus(struct event_base *base){
+    if(event_base_got_exit(base)){
+        printf("exit...\n");
+    }else if(event_base_got_break(base)){
+        printf("break...\n");
+    }
+}
+
+
+/**
+ * event所具有的属性：
+ *      1. 关联的event_base
+ *      2. 关联的文件描述符
+ *      3. 触发的条件
+ *      4. 关联的回调函数
+ *      5. 关联的回调函数中关联的指针参数
+ *      6. 设定的优先级
+*/
+void getEventProperty(struct event* event){
+    //返回为事件配置的event_base
+    struct event_base *event_base = event_get_base(event);
+
+    //获取为事件配置的文件描述符
+    evutil_socket_t fd = event_get_fd(event);
+
+    //返回时间的标志
+    short triggerEvents = event_get_events(event);
+
+    //返回事件配置的回调函数
+    event_callback_fn fn = event_get_callback(event);
+
+    //返回回调函数中的参数指针
+    void* args = event_get_callback_arg(event);
+
+    //获取事件的优先级
+    int priority = event_get_priority(event);
+}
+
+
 
 void cb_func(evutil_socket_t fd, short what, void *arg){
-    const char *data = reinterpret_cast<const char*>(arg);
-    printf("got an event on socket %d: %s %s [%s]\n", 
+    //回调函数中指定的参数转换
+    const char *data = reinterpret_cast<const char *>(arg);
+    printf("got an event on socket  %d : %s %s [%s]\n",
                                     fd,
-                                    (what & EV_TIMEOUT) ? "timeout" : "",
                                     (what & EV_READ) ? "read" : "",
+                                    (what & EV_TIMEOUT) ? "timeout" : "",
                                     data);
+
     if(what & EV_READ){
-        char buffer[1024];
-        ssize_t size  = read(fd, buffer, 1024);
-        std::cout << "readContent: " << string(buffer) << std::endl;
+        char buffer[1024]{};
+        read(fd, buffer, 1024);
+        printf("readContent: %s\n", buffer);
     }
-    // event_del(ev);
 }
+
+void cb_func_eventDel(evutil_socket_t fd, short what, void *arg){
+    //回调函数中指定的参数转换
+    const char *data = reinterpret_cast<const char *>(arg);
+    printf("got an event on socket  %d : %s %s [%s]\n",
+                                    fd,
+                                    (what & EV_READ) ? "read" : "",
+                                    (what & EV_TIMEOUT) ? "timeout" : "",
+                                    data);
+
+    if(what & EV_READ){
+        char buffer[1024]{};
+        read(fd, buffer, 1024);
+        printf("readContent: %s\n", buffer);
+    }
+
+    //执行回调后，卸载事件，不再需要被触发
+    event_del(event);
+}
+
+
+void cb_func_loopExit(evutil_socket_t fd, short what, void *arg){
+    //回调函数中指定的参数转换
+    const char *data = reinterpret_cast<const char *>(arg);
+    printf("got an event on socket  %d : %s %s [%s]\n",
+                                    fd,
+                                    (what & EV_READ) ? "read" : "",
+                                    (what & EV_TIMEOUT) ? "timeout" : "",
+                                    data);
+
+    if(what & EV_READ){
+        char buffer[1024]{};
+        read(fd, buffer, 1024);
+        printf("readContent: %s\n", buffer);
+    }
+
+    //执行回调后，卸载事件，不再需要被触发
+    event_base_loopexit(base, nullptr);
+}
+
+
+void cb_func_loopBreak(evutil_socket_t fd, short what, void *arg){
+    //回调函数中指定的参数转换
+    const char *data = reinterpret_cast<const char *>(arg);
+    printf("got an event on socket  %d : %s %s [%s]\n",
+                                    fd,
+                                    (what & EV_READ) ? "read" : "",
+                                    (what & EV_TIMEOUT) ? "timeout" : "",
+                                    data);
+
+    if(what & EV_READ){
+        char buffer[1024]{};
+        read(fd, buffer, 1024);
+        printf("readContent: %s\n", buffer);
+    }
+
+    //执行回调后，卸载事件，不再需要被触发
+    event_base_loopbreak(base);
+}
+
 
 
 /**
@@ -77,26 +193,36 @@ void cb_func(evutil_socket_t fd, short what, void *arg){
  * 
 */
 
-
-int main(int argc, char* argv[]){
+void test(){
     struct timeval five_seconds = {5, 0};
     base = event_base_new();
     printf("method: %s\n", event_base_get_method(base));
 
-    ev = event_new(base, 0, EV_TIMEOUT | EV_READ | EV_PERSIST, cb_func, (void *)"reading event");
-    event_add(ev, &five_seconds);
-    
+    event = event_new(base, STDIN_FILENO, EV_TIMEOUT | EV_READ | EV_PERSIST, cb_func, (void *)"reading event");
+    event_add(event, &five_seconds);
+
     int i = 0;
     while(true){
-        std::cout << "execute time %d ..." << i++ << std::endl;
+        std::cout << "execute time ..." << i++ << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
         //没有则返回
         event_base_loop(base, EVLOOP_NONBLOCK);
+        printExitStatus(base);
         //至少有一个事件发生，执行后，没有更多的事件，则返回
-        event_base_loop(base, EVLOOP_ONCE);
+        // event_base_loop(base, EVLOOP_ONCE);
     }
-    
+
     std::cout << "-----end----" << std::endl;
+    printExitStatus(base);
+}
+
+
+
+int main(int argc, char* argv[]){
+   
+    test();
+
+
     return 0;
 }
 
