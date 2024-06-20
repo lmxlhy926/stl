7c4af55daf0e4d6eb1929cbd8303c9c4a01d3af4
lhy <784723153@qq.com>
2024-06-20 22:44:14 +0800

    python

--- script/python/01base/04class/extendClass.py
+++ script/python/01base/04class/extendClass.py
@@ -6,10 +6,11 @@ class person:
     weight = None
 
     def __init__(self, name, age, weight) -> None:
-        print("---peron 构造---")
+        print("---person 构造---")
         self.name = name
         self.age = age
         self.weight = weight
+        print("person: id(name): {}".format(id(self.name)))
     
     def __del__(self):
         print("---person 析构---")
@@ -29,6 +30,7 @@ class student(person):
         # person.__init__(self, name, age, weight)
         self.name = name + "-student"
         self.grade = grade
+        print("person: id(name):{}; student: id(name):{}".format(id(super().name), id(self.name)))
 
     def __del__(self):
         super().__del__()
@@ -36,18 +38,8 @@ class student(person):
         print("--student 析构")
         
 
-    def show1(self):
+    def show(self):
         print("--student: name:{}, grade:{}".format(self.name, self.grade))
-
-
-
-s = student("peter", 10, 100, 99)
-s.show()
-
-
-
-
-
 
 
 
