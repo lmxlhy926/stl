3316ecd99f0b5f9e3480a2ebf5eb72b91bb23fc1
lhy <784723153@qq.com>
2025-03-09 23:35:48 +0800

    httptest

--- unix/test/main.cpp
+++ unix/test/main.cpp
@@ -1,47 +1,51 @@
+
 #include "httplib.h"
-#include <iostream>
-#include <thread>
 
 using namespace httplib;
 
 
-void client_func(){
-    httplib::Client cli("http://127.0.0.1:55555");
+int main(int argc, char* argv[]){
 
-    std::cout << "---------" << std::endl;
+  httplib::Server svr_;
 
-    auto res = cli.Get("/hi");
+  svr_.set_mount_point("/", "./www");
+  svr_.set_mount_point("/mount", "./www2");
+  svr_.set_file_extension_and_mimetype_mapping("abcde", "text/abcde");
 
-    std::cout << "res->status: " << res->status << std::endl;
 
-    std::cout << "res->body: " << res->body << std::endl;
+  svr_.Get("/hi",
+    [&](const Request & /*req*/, Response &res) {
+      res.set_content("Hello World!", "text/plain");
+    })
+
+  .Get("/file_content",
+      [&](const Request & /*req*/, Response &res) {
+        res.set_file_content("/mnt/d/ABP-341.mp4");
+      })
+
+  .Get("/file_content_with_content_type",
+      [&](const Request & /*req*/, Response &res) {
+        res.set_file_content("./www/file", "text/plain");
+      })
+      
+  .Get("/invalid_file_content",
+      [&](const Request & /*req*/, Response &res) {
+        res.set_file_content("./www/dir/invalid_file_path");
+      });
+
+
+  svr_.listen("0.0.0.0", 9999);
+
+
+  return true;
 }
 
 
-void server_func(){
-    httplib::Server svr;
-
-    svr.Get("/hi", [](const httplib::Request& request, httplib::Response& res){
-        res.set_content("hello world", "text/plain");
-    });
-
-    svr.listen("0.0.0.0", 55555);
-}
 
 
-int main(int argc, char* argv[])
-{
-    std::thread th1(server_func);
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    std::thread th2(client_func);
 
-    th1.join();
-    th2.join();
-   
-    return 0;
-}
 
 
 
